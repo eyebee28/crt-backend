38ca2d19cb97741102abe1ca2d1c30c7e3fee417
ivyleonelle <57502237+ivyleonelle@users.noreply.github.com>
2019-11-08 02:50:50 +0800

    crt-backend-helloworld

---
+++ crt-backend/src/main/java/com/ibm/crtbackend/CrtBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.crtbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrtBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrtBackendApplication.class, args);
+	}
+
+}
---
+++ crt-backend/src/test/java/com/ibm/crtbackend/CrtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.crtbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrtBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
